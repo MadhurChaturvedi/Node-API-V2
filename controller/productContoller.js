ab88207df06319bdd8daaf0f59a4aed123992087
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-05-06 19:38:54 +0530

    make changes

--- controller/productContoller.js
+++ controller/productContoller.js
@@ -4,7 +4,7 @@ const asyncHandler = require('express-async-handlr')
 // get All Product
 const getAllProducts = async (req, res) => {
     try {
-        const products = await Product.find({})
+        const products = await Product.find()
         res.status(200).json(products)
     }
     catch (err) {
