c4f947e45b2b5daf26c52814243861456acb7187
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-05-06 20:30:53 +0530

    change the env Fronet end url

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.use(errorMiddleware)
 
 const port = process.env.PORT
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}🎃`)
 })
